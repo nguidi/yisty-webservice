068ac29bf329a4015a05ff207b10c257a90cc235
Neri <nguidi@frd.utn.edu.ar>
2021-06-12 20:09:10 -0300

    Ups

--- src/mailer.js
+++ src/mailer.js
@@ -8,7 +8,7 @@ var transporter = nodemailer.createTransport(
             host: 'smtp.gmail.com',
             auth: {
                 user: 'yistyapp@gmail.com',
-                pass: 'Yisty911!'
+                pass: ''
             }
         }
     )
